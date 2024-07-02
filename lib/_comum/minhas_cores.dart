e95a93f1de9c3f0dd405037352a3c4a2f84b234c
gabrielmaster <gabrielmasterx2@gmail.com>
2024-07-02 17:36:29 -0300

    KLKI-1

--- lib/_comum/minhas_cores.dart
+++ lib/_comum/minhas_cores.dart
@@ -1,9 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class MinhasCores {
   static const Color cinza = Color(0xFF1E1E1E);
   static const Color rosa = Color(0xFF981DB9);
   static const Color azul = Color(0xFF0F76CE);
   static const Color branco = Color(0xFFFFFFFF);
-  static const Color cinza_claro = Color(0xFF666F7B);
+  static const Color cinza_Claro = Color(0xFF666F7B);
+  static const LinearGradient gradiente =
+      LinearGradient(colors: [Color(0xFF981DB9), Color(0xFF0F76CE)]);
 }
--- lib/pages/faixa_etaria.dart
+++ lib/pages/faixa_etaria.dart
@@ -1,5 +1,10 @@
+import 'dart:ui';
+
 import 'package:desempenho_esportivo/_comum/minhas_cores.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class FaixaEtaria extends StatelessWidget {
@@ -17,13 +22,122 @@ class FaixaEtaria extends StatelessWidget {
               fit: BoxFit.fill,
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text('faixa etaria'),
-          ),
           Column(
             children: [
-              OutlinedButton(onPressed: () {}, child: const Text('data'))
+              const Padding(
+                padding: EdgeInsets.only(top: 104.0, left: 40),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: SizedBox(
+                    height: 52,
+                    width: 309,
+                    child: Text(
+                      '''QUAL FAIXA ETARIA?''',
+                      style: TextStyle(
+                        color: MinhasCores.branco,
+                        fontFamily: 'StretchPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 115.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 16.0, left: 24, right: 24),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(13)),
+                            minimumSize: const Size(341.02, 62.47),
+                            textStyle: const TextStyle(
+                              fontSize: 30,
+                            ),
+                            side: const BorderSide(width: 1),
+                          ),
+                          child: const Text(
+                            'SUB 9',
+                            style: TextStyle(
+                                color: MinhasCores.branco,
+                                fontFamily: 'Outfit-Regular'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 16.0, left: 24, right: 24),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(13)),
+                              minimumSize: const Size(341.02, 62.47),
+                              textStyle: const TextStyle(
+                                fontSize: 30,
+                              ),
+                              side: const BorderSide(
+                                width: 1,
+                              )),
+                          child: const Text(
+                            'SUB 10',
+                            style: TextStyle(
+                                color: MinhasCores.branco,
+                                fontFamily: 'Outfit-Regular'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 16.0, left: 24, right: 24),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(13)),
+                              minimumSize: const Size(341.02, 62.47),
+                              textStyle: const TextStyle(
+                                fontSize: 30,
+                              ),
+                              side: const BorderSide(width: 1)),
+                          child: const Text(
+                            'SUB 11',
+                            style: TextStyle(
+                                color: MinhasCores.branco,
+                                fontFamily: 'Outfit-Regular'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 16.0, left: 24, right: 24),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(13)),
+                              minimumSize: const Size(341.02, 62.47),
+                              textStyle: const TextStyle(
+                                fontSize: 30,
+                              ),
+                              side: const BorderSide(width: 1)),
+                          child: const Text(
+                            'SUB 9',
+                            style: TextStyle(
+                                color: MinhasCores.branco,
+                                fontFamily: 'Outfit-Regular'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           )
         ],
